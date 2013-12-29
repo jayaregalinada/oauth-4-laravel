5c55f185a715a40d1fcae34aa802113b499bd6e9
Jay Are <jayaregalinada@gmail.com>
2013-12-30 03:40:15 +0800

    Edit config.php

--- src/config/config.php
+++ src/config/config.php
@@ -2,7 +2,7 @@
 
 return array( 
 	
-	/*
+	/**
 	|--------------------------------------------------------------------------
 	| oAuth Config
 	|--------------------------------------------------------------------------
@@ -10,10 +10,8 @@ return array(
 
 	/**
 	 * Storage
-	 *
-	 * Session | LaravelSession
 	 */
-	'storage' => 'LaravelSession', 
+	'storage' => 'ThomasweltonL4Session',
 
 	/**
 	 * Consumers
@@ -21,13 +19,41 @@ return array(
 	'consumers' => array(
 
 		/**
-		 * Facebook
+		 |******************************
+		 | Facebook
+		 |*******************************
+		 *
+		 * Create a new app - https://developers.facebook.com/apps
+		 *
+		 * @client_id - Facebook calls this the App ID
+		 * @client_secret - Facebook calls this the App Secret
+		 * @scope - Comma seperated string - See https://developers.facebook.com/docs/reference/login/
+		 *
 		 */
         'Facebook' => array(
             'client_id'     => '',
             'client_secret' => '',
             'scope'         => array(),
-        ),		
+        ),
+        /**
+	     |******************************
+	     | Google
+	     |******************************
+	     *
+	     * Create a new app - https://developers.google.com/+/quickstart/php
+	     * When creating the app set the redirect URI to be yousite.com/oauth/google/
+	     *
+	     * @client_id - Google calls this the Client ID
+	     * @client_secret - Google calls this the Client Secret
+	     * @scope - Comma seperated string - See https://developers.google.com/accounts/docs/OAuth2Login
+	     *
+	     */
+        'Google' => array(
+        	'client_id' => '',
+        	'client_secret' => '',
+        	'scope' => 'openid email'
+
+        ),
 
 	)
 
